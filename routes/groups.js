0f6f7a2401c2991fdc132a5fdee340be508191dc
hyeg0121 <w2218@e-mirim.hs.kr>
2023-10-25 10:25:44 +0900

    fix: api path 변경

--- routes/groups.js
+++ routes/groups.js
@@ -5,7 +5,7 @@ const db = require('../db/db'); // db.js 모듈 가져오기
 router.use(express.json());
 router.use(express.urlencoded({ extended: true }));
 
-router.post('/groups', (req, res) => {
+router.post('/', (req, res) => {
     const { creator_id, group_name, group_description, allow_search, group_goal, goal_name, operation_type,member_count, max_members } = req.body;
   
     const sql = 'INSERT INTO user_group (creator_id, group_name, group_description, allow_search, group_goal, goal_name, operation_type, member_count, max_members)' +
@@ -20,7 +20,7 @@ router.post('/groups', (req, res) => {
         console.log('그룹 생성 성공');
         res.status(201).json({ message: '그룹 생성 성공' , group: req.body});
       }
-    });
+    }); 
 });
 
 module.exports = router;
