fd1e1d73668c23b37074f83910b6d5a90cc12882
hyeg0121 <w2218@e-mirim.hs.kr>
2023-10-25 09:19:15 +0900

    feat: 그룹 생성하는 api 구현

--- api.js
+++ api.js
@@ -86,6 +86,25 @@ app.get('/users/:id', (req, res) => {
   });
 });
 
+//group 
+app.post('/groups', (req, res) => {
+  const { creator_id, group_name, group_description, allow_search, group_goal, goal_name, operation_type,member_count, max_members } = req.body;
+
+  const sql = 'INSERT INTO user_group (creator_id, group_name, group_description, allow_search, group_goal, goal_name, operation_type, member_count, max_members)' +
+  'VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?)';
+
+  db.query(sql, [creator_id, group_name, group_description, allow_search, group_goal, goal_name, operation_type, member_count, max_members], (err, result) => {
+    if (err) {
+      console.error('그룹 생성 오류:', err);
+      res.status(500).json({ message: '그룹 생성 실패' });
+    } else {
+      
+      console.log('그룹 생성 성공');
+      res.status(201).json({ message: '그룹 생성 성공' , group: req.body});
+    }
+  });
+});
+
 
 
 app.listen(port, () => {
