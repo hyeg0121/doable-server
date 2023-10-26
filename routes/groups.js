a0a0fbd9e924a29430337057a7520fdd2a239332
hyeg0121 <w2218@e-mirim.hs.kr>
2023-10-26 21:39:50 +0900

    feat: 사용자가 그룹 만들면 group_member 테이블에 값 추가

--- routes/groups.js
+++ routes/groups.js
@@ -6,21 +6,35 @@ router.use(express.json());
 router.use(express.urlencoded({ extended: true }));
 
 router.post('/', (req, res) => {
-    const { creator_id, group_name, group_description, allow_search, group_goal, goal_name, operation_type,member_count, max_members } = req.body;
-  
-    const sql = 'INSERT INTO user_group (creator_id, group_name, group_description, allow_search, group_goal, goal_name, operation_type, member_count, max_members)' +
-    'VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?)';
-  
-    db.query(sql, [creator_id, group_name, group_description, allow_search, group_goal, goal_name, operation_type, member_count, max_members], (err, result) => {
-      if (err) {
-        console.error('그룹 생성 오류:', err);
-        res.status(500).json({ message: '그룹 생성 실패' });
-      } else {
-        
-        console.log('그룹 생성 성공');
-        res.status(201).json({ message: '그룹 생성 성공' , group: req.body});
-      }
-    }); 
+  const { creator_id, group_name, group_description, allow_search, group_goal, goal_name, operation_type,member_count, max_members } = req.body;
+
+  const sql = 'INSERT INTO user_group (creator_id, group_name, group_description, allow_search, group_goal, goal_name, operation_type, member_count, max_members)' +
+  'VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?)';
+
+  db.query(sql, [creator_id, group_name, group_description, allow_search, group_goal, goal_name, operation_type, member_count, max_members], (err1, result1) => {
+    if (err) {
+      console.error('그룹 생성 오류:', err1);
+      res.status(500).json({ message: '그룹 생성 실패' });
+    } else {
+      const now = new Date();
+      const joinDate = now.getFullYear() + ' - ' + (now.getMonth() + 1) + ' - ' + now.getDate() // 가입 날짜
+
+      const sql = 'INSERT INTO group_members (user_id, group_id, join_date) VALUES (?, ?, ?)';
+      db.query(sql, [creator_id, result2.id, joinDate], (err2, result2) => {
+        if (err2) {
+          console.error('멤버 추가 오류:', err2);
+          res.status(500).json({ message: '멤버 추가 오류' });
+        } else {
+          console.log('멤버 추가 성공');
+          res.status(201).json({ group: req.body, group_members: result2});
+        }
+      });
+
+      console.log('그룹 생성 성공');
+    }
+  }); 
 });
+
+
 
 module.exports = router;
