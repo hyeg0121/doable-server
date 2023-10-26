f923f176bda0a0987df08f626952640b2345dbcb
hyeg0121 <w2218@e-mirim.hs.kr>
2023-10-26 22:31:16 +0900

    feat: todo router 추가

--- app.js
+++ app.js
@@ -9,10 +9,12 @@ const bodyParser = require('body-parser');
 const usersRouter = require('./routes/users');
 const groupsRouter = require('./routes/groups');
 const authRouter = require('./routes/auth');
+const todosRouter = require('./routes/todos');
 
 app.use('/users', usersRouter);
 app.use('/groups', groupsRouter);
 app.use('/auth', authRouter);
+app.use('/todos', todosRouter);
 
 app.use(cors({
   origin: '*', // 모든 출처 허용 옵션. true 를 써도 된다.
