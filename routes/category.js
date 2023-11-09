f6c153eac594e6274ad8d22d83f57fd4014612f0
hyeg0121 <w2218@e-mirim.hs.kr>
2023-11-09 22:36:19 +0900

    feat: 카테고리 만드는 api 생성

---
+++ routes/category.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const router = express.Router();
+const db = require('../db/db');
+const corsMiddleware = require('../middleware/cors');
+
+router.use(express.json());
+router.use(express.urlencoded({ extended: true }));
+router.use(corsMiddleware);
+
+router.post('/', (req, res) => {
+    const { user_id, color_code, category_name } = req.body;
+
+    db.query(
+        `INSERT INTO category (user_id, color_code, category_name) VALUES (? ,?, ?)`,
+        [user_id, color_code, category_name],
+        (err, results) => {
+            if (err) {
+                res.json({ message: '카테고리 생성 실패'});
+            } else {
+                res.json({ message: '카테고리 생성 성공'});
+            }
+        }
+    );
+});
+
+module.exports = router;
