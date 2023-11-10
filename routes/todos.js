50a34d02648111ce0fba082a884a2e2bd4786636
hyeg0121 <w2218@e-mirim.hs.kr>
2023-11-10 14:34:49 +0900

    feat: todo 데이터에  member 정보 추가

--- routes/todos.js
+++ routes/todos.js
@@ -1,9 +1,11 @@
 const express = require('express');
 const router = express.Router();
 const db = require('../db/db');
+const corsMiddleware = require('../middleware/cors');
 
 router.use(express.json());
 router.use(express.urlencoded({ extended: true }));
+router.use(corsMiddleware);
 
 // todo 만드는 api
 router.post('/:userNo', (req, res) => {
@@ -25,7 +27,7 @@ router.post('/:userNo', (req, res) => {
   ];
 
   // todo 테이블에 insert
-  db.query(`INSERT INTO todo (user_no, category, todo_name, operation_type, starting_value, unit, increase_decrease_option, increase_decrease_value, start_date, end_date, completed) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)`, todoData, (err, result) => {
+  db.query(`INSERT INTO todo (user_no, category_id, todo_name, operation_type, starting_value, unit, increase_decrease_option, increase_decrease_value, start_date, end_date, completed) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)`, todoData, (err, result) => {
     if (err) {
       console.error('Todo 생성 에러', err);
       res.status(500).json({ message: 'Todo 생성 실패' });
@@ -50,12 +52,29 @@ router.get('/', (req, res) => {
 
 router.get('/:userNo', (req, res) => {
   const userNo = req.params.userNo;
-  db.query(`SELECT * FROM todo WHERE user_id = ?`, [userNo], (err, results) => {
-    if (err) {
-      res.status(500).json({ message: 'todo 조회 실패' });
-    } else {
-      res.status(200).json({ results });
-    }
-  });
+  db.query(
+    `SELECT todo.*, category.category_name, category.color_code FROM todo
+    INNER JOIN category ON todo.category_id = category.id
+    WHERE todo.user_no = ?;`,
+    [userNo],
+    (err, results) => {
+      if (err) {
+        res.status(500).json({ message: 'todo 조회 실패' });
+      } else {
+        // 여기에서 결과를 가공하여 원하는 형식으로 반환
+        const formattedResults = results.map(result => {
+          return {
+            category: {
+              category_name: result.category_name,
+              color_code: result.color_code
+            },
+            // todo의 다른 필드 추가
+            todo_no: result.id,
+            todo_name: result.todo_name
+          };
+        });
+        res.status(200).json(formattedResults);
+      }
+    });
 });
 module.exports = router;
